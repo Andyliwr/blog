4e95619863b4b6970aa6e8bf9b319e64289a1680
andyliwr <andyliwr@outlook.com>
2017-09-04 22:45:17 +0800

    完成心声页面访问控制

--- source/scripts/whisper.js
+++ source/scripts/whisper.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
+    // 判断是否已经登录
+    if(getCookie('token')){
+      $('#no-login').hide()
+      $('#has-login').show()
+      getList(1, 10)
+    }else{
+      $('#no-login').hide()
+      $('#has-login').show() 
+    }
     $('#access_password').keypress(function(event){
         var keynum = (event.keyCode ? event.keyCode : event.which);
         if(keynum == '13'){
@@ -8,12 +17,60 @@ $(document).ready(function(){
                 url: 'http://blogapi.andylistudio.com/access?pwd='+pwd,
                 timeout: 10000,
                 success: function(res){
-                    console.log(res)
+                    if(res.code === 0){
+                        setCookie('token', res.token)
+                        // 获取心声列表
+                        getList(1, 10)
+                    }else{
+                        alert('密码错误, ' + res.msg)
+                    }
                 },
                 error: function(err){
                     console.log(err)
+                    alert('密码错误')
                 }
             })
         }
     });
 })
+
+function setCookie(name,value)
+{
+    var Days = 1;
+    var exp = new Date();
+    exp.setTime(exp.getTime() + Days*24*60*60*1000);
+    document.cookie = name + "="+ escape (value) + ";expires=" + exp.toGMTString();
+}
+
+function getCookie(name)
+{
+    var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
+    if(arr=document.cookie.match(reg)){
+        return unescape(arr[2]);
+    }else{
+        return null;
+    }
+}
+
+function getList(pagenum, limit){
+    $.ajax({
+        method: 'GET',
+        url: 'http://blogapi.andylistudio.com/getlist?pagenum='+pagenum+'&limit='+limit,
+        timeout: 10000,
+        success: function(res){
+            // 设置dom
+            if(res.code === 0){
+                let listHtml = ''
+                res.lists.forEach(function(item){
+                    listHtml += '<li><a class="title" href="item.link">'+item.title+'</a><span class="date">'+item.date+'</span></li>'
+                })
+                $('#has-login').html('<ul class="all-list">'+listHtml+'</ul>')
+            }else{
+                alert('获取心声列表失败')
+            }
+        },
+        error: function(err){
+            console.log(err)
+        }
+    })
+}
--- source/whisper/index.md
+++ source/whisper/index.md
@@ -7,6 +7,10 @@ date: 2017-09-03 18:01:42
 > 可惜终究没遇到那个愿意不厌其烦听自己述说的人
 > 她什么时候才会出现在我的生命里呢?
 
-<p style="color: red; font-size: 16px; text-align: center">对不起!, 此内容保密.</p>
-<p style="color: red; font-size: 16px; text-align: center">访问密码: <input id="access_password" type="password" size="12" /></p>
+<div id="no-login">
+  <p style="color: red; font-size: 16px; text-align: center; padding: 4px; margin: 0">对不起!, 此内容保密.</p>
+  <p style="color: red; font-size: 16px; text-align: center; padding: 4px; margin: 0">访问密码: <input id="access_password" type="password" size="12" /></p>
+</div>
+<div id="has-login">
+</div>
 <script type="text/javascript" src="../scripts/whisper.js" defer="defer"></script>
