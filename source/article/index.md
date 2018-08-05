06f81d989029a2f17489aafd9760ff85288ec2c1
DiKang Li <121960425@qq.com>
2018-08-05 10:18:03 +0800

    新增几篇随记

--- source/article/index.md
+++ source/article/index.md
@@ -34,33 +34,33 @@ date: 2017-12-10 19:29:00
     </div>
     <div class="link-item">
       <a href="posts/kemuer.html" target="_blank">
-          <span class="title">科目二挂了</span>
-          <span class="des">我是不是一直都只是看起来很努力</span>
-        </a>
+        <span class="title">科目二挂了</span>
+        <span class="des">我是不是一直都只是看起来很努力</span>
+      </a>
     </div>
     <div class="link-item">
       <a href="posts/guo_qing_1.html" target="_blank">
-          <span class="title">国庆第一天</span>
-          <span class="des">西湖之旅</span>
-        </a>
+        <span class="title">国庆第一天</span>
+        <span class="des">西湖之旅</span>
+      </a>
     </div>
     <div class="link-item">
       <a href="posts/guo_qing_2.html" target="_blank">
-          <span class="title">国庆第二天</span>
-          <span class="des">铜仁，我到了</span>
-        </a>
+        <span class="title">国庆第二天</span>
+        <span class="des">铜仁，我到了</span>
+      </a>
     </div>
     <div class="link-item">
       <a href="posts/ru_ni_yi_ban_de_ren.html" target="_blank">
-          <span class="title">我希望有个如你一般的人</span>
-          <span class="des">如山间清爽的风，如古城温暖的光</span>
-        </a>
+        <span class="title">我希望有个如你一般的人</span>
+        <span class="des">如山间清爽的风，如古城温暖的光</span>
+      </a>
     </div>
     <div class="link-item">
       <a href="posts/the_feelling_loving.html" target="_blank">
-          <span class="title">我是时候离开了</span>
-          <span class="des">我从来都是只是个指路人，只不过现在导航结束了而已</span>
-        </a>
+        <span class="title">我是时候离开了</span>
+        <span class="des">我从来都是只是个指路人，只不过现在导航结束了而已</span>
+      </a>
     </div>
     <!--<div class="link-item">
       <a href="posts/xi_da_li_bie.html" target="_blank">
@@ -70,15 +70,33 @@ date: 2017-12-10 19:29:00
     </div>-->
     <div class="link-item">
       <a href="posts/go_to_shanghai2.html" target="_blank">
-          <span class="title">出发，前往上海(二)</span>
-          <span class="des">人生已经如此艰难，有些事情就不要拆穿</span>
-        </a>
+        <span class="title">出发，前往上海(二)</span>
+        <span class="des">人生已经如此艰难，有些事情就不要拆穿</span>
+      </a>
     </div>
     <div class="link-item">
       <a href="posts/go_to_shanghai.html" target="_blank">
-          <span class="title">出发，前往上海</span>
-          <span class="des">既然选择了远方，留下的就只能是背影了</span>
-        </a>
+        <span class="title">出发，前往上海</span>
+        <span class="des">既然选择了远方，留下的就只能是背影了</span>
+      </a>
+    </div>
+    <div class="link-item">
+      <a href="posts/memory.html" target="_blank">
+        <span class="title">回忆</span>
+        <span class="des">我等的人你在哪</span>
+      </a>
+    </div>
+    <div class="link-item">
+      <a href="posts/plan_is_not_realiable.html" target="_blank">
+        <span class="title">计划赶不上变化</span>
+        <span class="des">生活就像巧克力</span>
+      </a>
+    </div>
+    <div class="link-item">
+      <a href="posts/yu_zhong_kuang_ben.html" target="_blank">
+        <span class="title">雨中狂奔</span>
+        <span class="des">我想找个简单，温柔，愿意跟随我一辈子的女孩</span>
+      </a>
     </div>
   </div>
 </div>
@@ -87,9 +105,15 @@ date: 2017-12-10 19:29:00
   <div class="content">
     <div class="link-item">
       <a href="posts/dang_ni_lao_le.html" target="_blank">
-          <span class="title">当你老了</span>
-          <span class="des">When you are old and grey and full of sleep, and nodding by the fire, take down this book.</span>
-        </a>
+        <span class="title">当你老了</span>
+        <span class="des">When you are old and grey and full of sleep, and nodding by the fire, take down this book.</span>
+      </a>
+    </div>
+    <div class="link-item">
+      <a href="posts/slience.html" target="_blank">
+        <span class="title">默</span>
+        <span class="des">你是愿意倾听我内心的苦楚</span>
+      </a>
     </div>
   </div>
 </div>
--- source/scripts/loadListCss.js
+++ source/scripts/loadListCss.js
@@ -2,71 +2,71 @@
 var dynamicLoading = {
   css: function(path) {
     if (!path || path.length === 0) {
-      throw new Error('argument "path" is required !');
+      throw new Error('argument "path" is required !')
     }
-    var head = document.getElementsByTagName('head')[0];
-    var link = document.createElement('link');
-    link.href = path;
-    link.rel = 'stylesheet';
-    link.type = 'text/css';
-    head.appendChild(link);
+    var head = document.getElementsByTagName('head')[0]
+    var link = document.createElement('link')
+    link.href = path
+    link.rel = 'stylesheet'
+    link.type = 'text/css'
+    head.appendChild(link)
   },
   js: function(path) {
     if (!path || path.length === 0) {
-      throw new Error('argument "path" is required !');
+      throw new Error('argument "path" is required !')
     }
-    var head = document.getElementsByTagName('head')[0];
-    var script = document.createElement('script');
-    script.src = path;
-    script.type = 'text/javascript';
-    head.appendChild(script);
+    var head = document.getElementsByTagName('head')[0]
+    var script = document.createElement('script')
+    script.src = path
+    script.type = 'text/javascript'
+    head.appendChild(script)
   }
-};
-dynamicLoading.css('../styles/list.css');
-dynamicLoading.js('//fs.andylistudio.com/blog/js/v2/sweetalert2.all.min.js');
+}
+dynamicLoading.css('../styles/list.css')
+// dynamicLoading.js('//fs.andylistudio.com/blog/js/v2/sweetalert2.all.min.js');
 window.onload = function() {
   // 去掉container里的换行符
-  var br = document.querySelectorAll('.container br');
+  var br = document.querySelectorAll('.container br')
   br.forEach(function(item) {
-    item.remove();
-  });
-  if (window.location.href.indexOf('/article') > 0) {
-    swal({
-      title: '随心而动',
-      input: 'text',
-      inputAttributes: {
-        autocapitalize: 'off'
-      },
-      showCancelButton: false,
-      confirmButtonText: '确认',
-      showLoaderOnConfirm: true,
-      allowEscapeKey: false,
-      allowOutsideClick: false
-    }).then(result => {
-      if (result.value === '随刃而行') {
-        swal({
-          type: 'success',
-          title: '温馨提示',
-          text: '您已经通过认证'
-        });
-      } else {
-        swal({
-          type: 'error',
-          title: '温馨提示',
-          text: '认证失败，即将返回首页',
-          timer: 2000,
-          onOpen: () => {
-            swal.showLoading();
-          }
-        }).then(result => {
-          if (
-            // Read more about handling dismissals
-            result.dismiss === swal.DismissReason.timer
-          ) {
-            window.location.href = '/';
-          }
-        });
-      }
-    });
-  }
-};
+    item.remove()
+  })
+  // if (window.location.href.indexOf('/article') > 0) {
+  //   swal({
+  //     title: '随心而动',
+  //     input: 'text',
+  //     inputAttributes: {
+  //       autocapitalize: 'off'
+  //     },
+  //     showCancelButton: false,
+  //     confirmButtonText: '确认',
+  //     showLoaderOnConfirm: true,
+  //     allowEscapeKey: false,
+  //     allowOutsideClick: false
+  //   }).then(result => {
+  //     if (result.value === '随刃而行') {
+  //       swal({
+  //         type: 'success',
+  //         title: '温馨提示',
+  //         text: '您已经通过认证'
+  //       });
+  //     } else {
+  //       swal({
+  //         type: 'error',
+  //         title: '温馨提示',
+  //         text: '认证失败，即将返回首页',
+  //         timer: 2000,
+  //         onOpen: () => {
+  //           swal.showLoading();
+  //         }
+  //       }).then(result => {
+  //         if (
+  //           // Read more about handling dismissals
+  //           result.dismiss === swal.DismissReason.timer
+  //         ) {
+  //           window.location.href = '/';
+  //         }
+  //       });
+  //     }
+  //   });
+  // }
+}
